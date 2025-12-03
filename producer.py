2932e20cc78c94b44eaf807bd1036c4fef0b690c
Shrirang-Bhagat <shribh3108@gmail.com>
2025-12-03 09:24:37 +0530

    added docker file and producer

---
+++ producer.py
@@ -0,0 +1,34 @@
+import uuid
+import json
+from confluent_kafka import Producer
+
+producer_config = {
+    'bootstrap.servers': 'localhost:9092',
+}
+
+producer = Producer(producer_config)
+
+def delivery_report(err, msg):
+    if err is not None:
+        print(f"Delivery failed for Order {msg.key()}: {err}")
+    else:
+        print(f"Order {msg.key()} successfully produced to {msg.topic()} [{msg.partition()}] at offset {msg.offset()}")
+        print(f"Message value: {msg.value().decode('utf-8')}")
+        print(dir(msg) )
+
+order = {
+    'order_id': str(uuid.uuid4()),
+    'user': 'John Doe',
+    'item': 'mushrooms',
+    'quantity': 10
+}
+
+
+value = json.dumps(order).encode('utf-8')
+
+producer.produce(
+    topic='orders', 
+    value=value,
+    callback=delivery_report)
+
+producer.flush()
